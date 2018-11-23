744f26055a10a05af6cc94c12045de9e28e14928
Olena Rubanenko <alenaRubanenko27@gmail.com>
2018-11-23 15:41:29 +0200

    add test api call

--- client/src/app.js
+++ client/src/app.js
@@ -1,8 +1,22 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import axios from 'axios';
 import App from './App.vue';
 import '../assets/app.styl';
+
+const instance = axios.create({
+                                baseURL: 'http://localhost:8000/api',
+                                timeout: 1000,
+                                headers: {crossDomain: true}
+                              });
+instance.get('locations/locations')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 
 Vue.use(Vuetify, {
   iconfont: 'fa'
